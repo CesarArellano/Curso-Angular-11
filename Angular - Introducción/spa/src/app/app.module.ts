54e153d39e610e0c3671a9cfe93b5d2b99944862
CesarArellano <raywayday@gmail.com>
2020-12-10 19:01:40 -0600

    Configuracion de navbar con bootstrap (npm)

--- Angular - Introducción/spa/src/app/app.module.ts
+++ Angular - Introducción/spa/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/shared/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
