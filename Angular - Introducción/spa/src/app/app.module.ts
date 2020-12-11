b39ebadd55e568b43eff9ca784d8f2cd58ebda5f
CesarArellano <raywayday@gmail.com>
2020-12-10 23:06:41 -0600

    routerLink y routerLinkActive

--- Angular - Introducción/spa/src/app/app.module.ts
+++ Angular - Introducción/spa/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// Rutas
 
+import { APP_ROUTING } from './app.routes';
+// Componentes
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
-import { HomeComponent } from './components/shared/home/home.component';
+import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 
@@ -18,7 +21,8 @@ import { HeroesComponent } from './components/heroes/heroes.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
